c26da3bcfd8cc7c0c297a05fd0f883a9e70b1c14
Qiling Zou <zql0419@gmail.com>
2022-08-31 01:48:36 -0400

    Modify build.sh
    Former-commit-id: 6a113aef86fcd3b15d2339f2077fcb6243edd721

--- build.sh
+++ build.sh
@@ -6,14 +6,16 @@ echo "$1"
 # https://stackoverflow.com/questions/24174394/cmake-is-not-able-to-find-python-libraries
 # check generated CMakeCache.txt
 if [ $1 = "Release" ]; then
-  echo "Build release version"
+  echo "Build Release version"
   cmake -DCMAKE_BUILD_TYPE=$1 ../src \
+        -DCMAKE_CXX_FLAGS="-std=c++11 -m64 -Wall -O2" \
         -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
         -DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))") \
         -DPYTHON_EXECUTABLE:FILEPATH=`which python`
 else
-  echo "Build debug version"
+  echo "Build Debug version"
   cmake ../src \
+        -DCMAKE_CXX_FLAGS="-std=c++11 -m64 -Wall" \
         -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
         -DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))") \
         -DPYTHON_EXECUTABLE:FILEPATH=`which python`
